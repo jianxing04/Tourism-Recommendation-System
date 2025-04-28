f71c98a6e8e6c87b091e1188e01f67be009e2a4b
jianxing04 <146063224+jianxing04@users.noreply.github.com>
2025-04-28 23:14:47 +0800

    Add files via upload

--- Travel_System/blogwindow.cpp
+++ Travel_System/blogwindow.cpp
@@ -2,6 +2,8 @@
 #include <QDebug>
 #include<QSqlError>
 #include<QDialog>
+#include <QMap>
+#include<queue>
 BlogWindow::BlogWindow(QWidget *parent,QSqlDatabase DB)
     : QMainWindow(parent)
 {
@@ -55,6 +57,174 @@ BlogWindow::BlogWindow(QWidget *parent,QSqlDatabase DB)
 BlogWindow::~BlogWindow()
 {
     db.close();
+}
+
+
+// 比较器，用于优先队列
+struct Compare {
+    bool operator()(HuffmanNode* l, HuffmanNode* r) {
+        return l->freq > r->freq;
+    }
+};
+
+// 构建哈夫曼树
+HuffmanNode* BlogWindow::buildHuffmanTree(const QString& input) {
+    QMap<QChar, int> freqMap;
+    for (const QChar& c : input) {
+        freqMap[c]++;
+    }
+
+    std::priority_queue<HuffmanNode*, std::vector<HuffmanNode*>, Compare> minHeap;
+    for (const auto& pair : freqMap.asKeyValueRange()) {
+        minHeap.push(new HuffmanNode(pair.first, pair.second));
+    }
+
+    while (minHeap.size() != 1) {
+        HuffmanNode* left = minHeap.top();
+        minHeap.pop();
+        HuffmanNode* right = minHeap.top();
+        minHeap.pop();
+
+        HuffmanNode* top = new HuffmanNode('\0', left->freq + right->freq);
+        top->left = left;
+        top->right = right;
+
+        minHeap.push(top);
+    }
+
+    return minHeap.top();
+}
+
+// 生成哈夫曼编码表
+void BlogWindow::generateCodes(HuffmanNode* root, QString str, QMap<QChar, QString>& huffmanCode) {
+    if (!root) return;
+
+    if (!root->left && !root->right) {
+        huffmanCode[root->data] = str;
+    }
+
+    generateCodes(root->left, str + "0", huffmanCode);
+    generateCodes(root->right, str + "1", huffmanCode);
+}
+
+// 哈夫曼压缩函数
+QPair<QString, QMap<QChar, QString>> BlogWindow::huffmanCompress(const QString& input) {
+    HuffmanNode* root = buildHuffmanTree(input);
+    QMap<QChar, QString> huffmanCode;
+    generateCodes(root, "", huffmanCode);
+
+    QString compressed;
+    for (const QChar& c : input) {
+        compressed += huffmanCode[c];
+    }
+
+    return {compressed, huffmanCode};
+}
+
+// 哈夫曼解压缩函数
+QString BlogWindow::huffmanDecompress(const QString& compressed, const QMap<QChar, QString>& huffmanCode) {
+    QMap<QString, QChar> reverseCode;
+    for (const auto& pair : huffmanCode.asKeyValueRange()) {
+        reverseCode[pair.second] = pair.first;
+    }
+
+    QString decompressed;
+    QString currentCode;
+    for (const QChar& bit : compressed) {
+        currentCode += bit;
+        if (reverseCode.contains(currentCode)) {
+            decompressed += reverseCode[currentCode];
+            currentCode.clear();
+        }
+    }
+
+    return decompressed;
+}
+
+int BlogWindow::editDistance(const QString& s1, const QString& s2) {
+    int m = s1.length();
+    int n = s2.length();
+    std::vector<std::vector<int>> dp(m + 1, std::vector<int>(n + 1));
+
+    for (int i = 0; i <= m; ++i) {
+        for (int j = 0; j <= n; ++j) {
+            if (i == 0) {
+                dp[i][j] = j;
+            } else if (j == 0) {
+                dp[i][j] = i;
+            } else if (s1[i - 1] == s2[j - 1]) {
+                dp[i][j] = dp[i - 1][j - 1];
+            } else {
+                dp[i][j] = 1 + std::min({dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1]});
+            }
+        }
+    }
+    return dp[m][n];
+}
+
+// 计算 KMP 算法的部分匹配表
+std::vector<int> BlogWindow::computeLPS(const QString& pattern) {
+    int m = pattern.length();
+    std::vector<int> lps(m);
+    int len = 0;
+    lps[0] = 0;
+    int i = 1;
+    while (i < m) {
+        if (pattern[i] == pattern[len]) {
+            len++;
+            lps[i] = len;
+            i++;
+        } else {
+            if (len != 0) {
+                len = lps[len - 1];
+            } else {
+                lps[i] = 0;
+                i++;
+            }
+        }
+    }
+    return lps;
+}
+
+// KMP 字符串匹配函数
+bool BlogWindow::kmpSearch(const QString& text, const QString& pattern) {
+    int n = text.length();
+    int m = pattern.length();
+    std::vector<int> lps = computeLPS(pattern);
+    int i = 0;
+    int j = 0;
+    while (i < n) {
+        if (pattern[j] == text[i]) {
+            j++;
+            i++;
+        }
+        if (j == m) {
+            return true;
+        } else if (i < n && pattern[j] != text[i]) {
+            if (j != 0) {
+                j = lps[j - 1];
+            } else {
+                i++;
+            }
+        }
+    }
+    return false;
+}
+
+// 字符串匹配函数
+int BlogWindow::stringMatch(const QString& str1, const QString& str2) {
+    if (str1 == str2) {
+        return 1;
+    }
+
+    QString shorter = str1.length() < str2.length() ? str1 : str2;
+    QString longer = str1.length() < str2.length() ? str2 : str1;
+
+    if (kmpSearch(longer, shorter)) {
+        return 2;
+    }
+
+    return editDistance(str1, str2);
 }
 
 void BlogWindow::loadBlogs(bool sortByLikes, const QString& searchQuery )
--- Travel_System/blogwindow.h
+++ Travel_System/blogwindow.h
@@ -11,7 +11,16 @@
 #include <QLabel>
 #include<QLineEdit>
 #include<QHBoxLayout>
+#include<vector>
 
+// 哈夫曼树节点结构体
+struct HuffmanNode {
+    QChar data;
+    int freq;
+    HuffmanNode *left, *right;
+
+    HuffmanNode(QChar data, int freq) : data(data), freq(freq), left(nullptr), right(nullptr) {}
+};
 class BlogWindow : public QMainWindow
 {
     Q_OBJECT
@@ -36,5 +45,13 @@ private:
     QHBoxLayout *searchLayout;
     QLineEdit *searchInput;
     QPushButton *searchButton;
+    HuffmanNode* buildHuffmanTree(const QString& input);
+    void generateCodes(HuffmanNode* root, QString str, QMap<QChar, QString>& huffmanCode);
+    QPair<QString, QMap<QChar, QString>> huffmanCompress(const QString& input);
+    QString huffmanDecompress(const QString& compressed, const QMap<QChar, QString>& huffmanCode);
+    int editDistance(const QString& s1, const QString& s2);
+    std::vector<int> computeLPS(const QString& pattern);
+    bool kmpSearch(const QString& text, const QString& pattern);
+    int stringMatch(const QString& str1, const QString& str2);
 };
 #endif // BLOGWINDOW_H
